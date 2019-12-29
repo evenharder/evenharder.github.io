9dbe489971da85e31f2dbb6fb97635b6723cd5fd
Sangheon Lee <comp.evenharder@gmail.com>
2019-12-29 20:22:40 +0900

    Apply 'bundle exec jekyll build'
    I thought 'bundle exec jekyll serve' was okay...?

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,25 +2,25 @@ var store = [{
         "title": "블로그를 시작했습니다",
         "excerpt":"미루고 미루던 Jekyll 블로그 공사를 진행했습니다. 정말 Github Page와 Jekyll의 궁합이 좋긴 하네요. Minimal Mistakes도 정말 깔끔하고 좋은 테마입니다. Github Page + Jekyll 처음 써보시는 분이라면, 이 repo를 clone 떠서 바로 시작해보아도 좋을 정도입니다. 블로그에 대체 뭘 쓰면 좋을까 고민이 많았습니다. 알고리즘 강의. 제가 잘 모르는 내용이나, 이전에 동아리에서 다루었던...","categories": ["blog"],
         "tags": ["Jekyll"],
-        "url": "http://localhost:4000/blog/hello-jekyll/",
+        "url": "/blog/hello-jekyll/",
         "teaser":null},{
         "title": "Jekyll 기반 블로그 글꼴 수정하기",
         "excerpt":"Jekyll 같은 정적 웹페이지 생성기는 편하지만, 글꼴처럼 세부 설정을 건드리는 건 편하지 않습니다. 첫 포스트에서 추천드린 이 repo에는 _sass, _includes, _layouts 등 디렉토리가 없습니다. 그럼에도 불구하고 잘 작동하는 이유는 remote에 있는 설정을 그대로 가져다가 쓰기 때문입니다. 때문에, 이쪽 관련된 설정을 건드리는 건 얼핏 보기엔 쉬워 보이지 않습니다. 몇 시간에 걸친...","categories": ["blog"],
         "tags": ["Jekyll"],
-        "url": "http://localhost:4000/blog/jekyll-change-fonts/",
+        "url": "/blog/jekyll-change-fonts/",
         "teaser":null},{
         "title": "Sibelius Violin Concerto in D minor, Op. 47",
         "excerpt":"듣고 왜 이 협주곡을 이전에 알지 못했을까 싶었습니다.     이 NAVER 지식백과의 칼럼을 통해 배경과 음악적 느낌을 접하시면 될 것 같습니다.   연주 시간은 대략 27분에서 33분 정도입니다.       연주도 일품이지만, 막심 벤게로프의 표정 변화를 보며 웃음을 참지 못하는 분들도 있으리라 생각합니다.  ","categories": ["music"],
         "tags": ["classic"],
-        "url": "http://localhost:4000/music/sibelius-violin-concerto/",
+        "url": "/music/sibelius-violin-concerto/",
         "teaser":null},{
         "title": "블로그에 MathJax 추가",
         "excerpt":"수식을 보기 좋게 렌더링해주는 MathJax를 추가하였습니다.     $ e^{\\pi i} + 1 = 0$ 같이 인라인 수식도,     같은 수식도 쓸 수 있습니다.   이 포스트 의 도움을 받아 쉽게 추가할 수 있었습니다.   ","categories": ["blog"],
         "tags": ["Jekyll"],
-        "url": "http://localhost:4000/blog/add-mathjax/",
+        "url": "/blog/add-mathjax/",
         "teaser":null},{
         "title": "Persistent Segment Tree w/ BOJ 13538",
         "excerpt":"헷갈리던 Persistent Segment Tree를 드디어 이해했습니다. Persistent Segment Tree란, 크기 $N$의 segment tree가 총 $M$번 상태 변화가 일어날 때, 이를 공간 복잡도 $O(N + M \\lg N)$에 저장하는 자료구조입니다. 대표적인 예시로 2차원 격자에서, 점 갱신이 일어나지 않을 때 $x_1 \\leq x \\leq x_2$이면서 $y_1 \\leq y \\leq y_2$를 만족하는 점의...","categories": ["algo"],
         "tags": ["PS","Data Structure"],
-        "url": "http://localhost:4000/algo/persistent-segment-tree-xor-query/",
+        "url": "/algo/persistent-segment-tree-xor-query/",
         "teaser":null}]
